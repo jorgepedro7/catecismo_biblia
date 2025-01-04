a4637f157996ccc9d012f4d5ed54e966911f390d
Jorge Pedro <jorgepedro7@gmail.com>
2025-01-04 17:34:56 -0300

    update firebase-config.py private_key

--- firebase_config.py
+++ firebase_config.py
@@ -11,14 +11,14 @@ load_dotenv()
 # Inicializando o Firebase Admin SDK com as credenciais do arquivo JSON
 if not firebase_admin._apps:
     # Obtenha a chave privada e converta os caracteres escapados para linhas reais
-    private_key = os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
+    private_key = st.secrets["FIREBASE_PRIVATE_KEY"]
 
     # Construa o dicionário de credenciais
     firebase_credentials = {
         "type": "service_account",
         "project_id": os.getenv("FIREBASE_PROJECT_ID"),
         "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": private_key,
+        "private_key": private_key.replace("\\n", "\n"),
         "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
         "client_id": os.getenv("FIREBASE_CLIENT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
